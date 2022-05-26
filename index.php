ce35a7cb79f4503243507727d28ce8e9c88090d6
A1dar0n <106350742+A1dar0n@users.noreply.github.com>
2022-05-26 23:26:29 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+        <title>Accueil | SEIO - Junior-Entreprise</title>
+ 
+        <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css'>
+        <link rel="stylesheet" href="assets/css/css.css">
+    </head>
+  
+    <body>
+        <nav class="navbar navbar-expand-custom navbar-mainbg">
+            <div class="wrapper collapse navbar-collapse">
+                <div class="button">
+                    <div class="icon">
+                        <i class="fab fa-facebook-f"></i>
+                    </div>
+                    <span>Facebook</span>
+                </div>
+                <div class="button">
+                    <div class="icon">
+                        <i class="fab fa-twitter"></i>
+                    </div>
+                    <span>Twitter</span>
+                </div>
+                <div class="button">
+                    <div class="icon">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <span>Instagram</span>
+                </div>
+                <div class="button">
+                    <div class="icon">
+                        <i class="fab fa-linkedin"></i>
+                    </div>
+                    <span>LinkedIn</span>
+                </div>
+            </div>
+        
+            <button class="navbar-toggler" type="button" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <i class="fas fa-bars text-white"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <div class="hori-selector">
+                        <div class="left"></div>
+                        <div class="right"></div>
+                    </div>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="javascript:void(0);"><i class="fa fa-home"></i>Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);"><i class="fa fa-group"></i>Notre Equipe</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);"><i class="fa fa-compass-drafting"></i>Expertise</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);"><i class="fa fa-clipboard-list"></i>Références</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);"><i class="fa fa-graduation-cap"></i>Etudiant à l'ESEO</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);"><i class="fa fa-question"></i>FAQ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="javascript:void(0);"><i class="fa fa-message"></i>Nous Contacter</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        
+        <header>
+            <div class="left-intro">
+                <div>
+                    <img src=assets/img/logo_SEIO_color.png style="width:80%;"> 
+                </div>
+            </div>
+  
+            <div class="slider">
+                <ul>
+                    <li style="background-image:url(assets/img/study.png);">
+                        <div class="center-y">
+                            <h3 style="color:black">Étude du projet</h3>
+                        </div>
+                    </li>
+                    <li style="background-image:url(assets/img/write.png);">
+                        <div class="center-y">
+                            <h3 style="color:black">Rédaction du devis</h3>
+                        </div>
+                    </li>
+                    <li style="background-image:url(assets/img/developpement.png);">
+                        <div class="center-y">
+                            <h3 style="color:black">Développement et suivi d'étude</h3>
+                        </div>
+                    </li>
+                    <li style="background-image:url(assets/img/complete.png);">
+                        <div class="center-y">
+                            <h3 style="color:black">Livraison du projet</h3>
+                        </div>
+                    </li>
+                </ul>
+                <ul>
+                    <nav>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                        <a href="#"></a>
+                    </nav>
+                </ul>
+            </div>
+        </header>
+        
+        <section class="section-presentation">
+            <div class="content">
+                <h2 class="section-title" style="color:black;">
+                    SEIO
+                    <span>- Service d’Electronique et d’Informatique de l’Ouest</span>
+                </h2>
+                <h3 class="section-sub-title">
+                    /Présentation_
+                </h3>
+                
+                <p>
+                    Depuis 1983, le SEIO - Junior-Entreprise de l’ESEO à Angers - s’inscrit dans le mouvement international des Junior-Entreprises. Notre structure propose des projets dans les domaines de notre grande école d'ingénieur généraliste en électronique et informatique.
+                    La marque Junior-Entreprise représente aujourd’hui plus de 200 structures en France. Ces structures sont principalement implantées au sein de Grandes Écoles et Universités du territoire. Avec un chiffre d’affaire cumulé de 9 millions d'euros par an, les Junior-Entreprises permettent aux étudiants d'entrer en contact avec le monde professionnel tout en mettant en pratique leurs enseignements théoriques.
+                </p>
+                    
+                <p>
+                    Nous disposons d'étudiants et d'intervenants aux profils et aux compétences variées, forts d’un enseignement pluridisciplinaire lié aux NTIC, enseigné par des spécialistes dans leur domaine.
+                    Nous proposons nos services et notre savoir-faire aux professionnels dans tous nos domaines, de la réalisation de sites internet à la conception et au développement d’applications mobiles sur iOS et Android, en passant aussi par la conception de systèmes électroniques et de systèmes embarqués.
+                    Nos administrateurs sont là pour vous accompagner et vous conseiller dans le développement de vos projets.
+                </p>
+                <div class="warper_number">
+                    <div class="numbers numbers_left">
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--1" data-fake="8642519073">2</span>
+                        </span>
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--2" data-fake="5207186394">3</span>
+                        </span>
+                        <p>Junior-Entrepreneurs</p>
+                    </div>
+                    
+                    <div class="numbers numbers_center">
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--1" data-fake="8642519073">3</span>
+                        </span>
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--2" data-fake="5207186394">9</span>
+                        </span>
+                        <p>Ans d’expérience</p>
+                    </div>
+                    
+                    <div class="numbers numbers_right">
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--1" data-fake="8642519073">1</span>
+                        </span>
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--2" data-fake="5207186394">6</span>
+                        </span>
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--3" data-fake="8395216407">0</span>
+                        </span>
+                        <span class="numbers__window">
+                            <span class="numbers__window__digit numbers__window__digit--3" data-fake="5207186394">0</span>
+                        </span>
+                        <p>Elèves Ingénieurs</p>
+                    </div>
+                </div>
+            </div>       
+        </section>
+        
+        <section class="section-prix">
+            <div class="content">
+                <h2 class="section-title" style="color:black;">
+                    SEIO
+                </h2>
+                <h3 class="section-sub-title">
+                    /Prix de la meilleure étude d’ingénierie -  Année 2019_ 
+                </h3>
+                
+                <p>
+                    Avec plus d’une centaine d’équipes candidates, la Junior-Entreprise SEIO a remporté le Prix de la Meilleure Étude en Ingénierie lors du Congrès National d’Été 2019 de la Confédération Nationale des Junior-Entreprises (CNJE). En compétition avec des projets de Centrale Supelec et de l'Insa Lyon, le SEIO s’est démarqué par la qualité de son étude. Les candidats devaient présenter un dossier complet abordant le contexte d'une étude du mandat passé. Les étudiants de l’ESEO avaient choisi de mettre en avant un projet d'électronique permettant la détection des écarts d’ouverture des stalles de départ pour les courses de chevaux.
+                </p>
+            </div>
+        </section>
+        
+        <footer class="footer">
+            <div class="start-learning">
+                <div class="footer-start">
+                    <div class="texts">
+                        <h2 class="section-title">Nos partenaires</h2>
+                        <h3 class="section-sub-heading">
+                            <span>Ils nous font</span> <strong>confiances</strong>
+                        </h3>
+                    </div>
+                    <a href="#" >
+                        <img src="assets/img/BretagneConseil_White.png" alt="illustration" width="120" height="auto">
+                    </a>
+                    <a href="#" >
+                        <img src="assets/img/SG_blanc.png" alt="illustration" width="120" height="auto">
+                    </a>
+                    <a href="#" >
+                        <img src="assets/img/ALTEN_BLANC.png" alt="illustration" width="120" height="auto">
+                    </a>
+                    <a href="#" >
+                        <img src="assets/img/ESEO_White.png" alt="illustration" width="120" height="auto">
+                    </a>
+                    <a href="#" >
+                        <img src="assets/img/JE_blanc.png" alt="illustration" width="120" height="auto">
+                    </a>
+                </div>
+            </div>
+            
+            <div class="inner">
+                <div class="column is-logo">
+                    <a href="#" class="main-logo">
+                        <div class="logo">
+                            <img src="assets/img/logo_SEIO_white_head.png" alt="SEIO">
+                        </div>
+                        <div class="logo-info">
+                            <div class="text">SEIO</div>
+                            <span class="copyright">© 2022. Tout droit réservé.</span>
+                        </div>
+                    </a>
+                </div>
+                <div class="column is-nav">
+                    <div class="column-title">Navigation</div>
+                        <ul>
+                            <li><a href="#">Accueil</a></li>
+                            <li><a href="#">Notre Equipe</a></li>
+                            <li><a href="#">Expertise</a></li>
+                            <li><a href="#">Référence</a></li>
+                            <li><a href="#">Etudiant à l'ESEO</a></li>
+                            <li><a href="#">FAQ</a></li>
+                            <li><a href="#">Nous Contacter</a></li>
+                        </ul>
+                    </div>
+                <div class="column is-nav">
+                    <div class="column-title">Contact</div>
+                    <ul>
+                        <li><a href="#"><i class="fa fa-envelope-open"></i>contact@seio.org</a></li>
+                        <li><a href="#"><i class="fa fa-phone"></i>06 03 52 11 19</a></li>
+                        <li><a href="#"><i class="fa fa-location-dot"></i>10 Boulevard Jean Jeanneteau - 49107 Angers</a></li>
+                        <li><a href="#"><i class="fa fa-circle-info"></i>Mentions légales</a></li>
+                    </ul>
+                </div>
+                <div class="column is-nav">
+                    <div class="column-title">Pour aller plus loin</div>
+                    <ul>
+                        <li><a href="#">Notre plaquette</a></li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+        
+        <script src='https://code.jquery.com/jquery-3.4.1.min.js'></script>
+        <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js'></script>
+        <script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js'></script>
+        <script src="https://kit.fontawesome.com/3d87623f9d.js" crossorigin="anonymous"></script>
+        <script src="assets/js/script_slider.js"></script>
+        <script src="assets/js/script_navbar.js"></script>
+    </body>
+</html>
